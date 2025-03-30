5202f0d2c769f1936fa4ecf33659e5e9e3910e95
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 20:37:04 +0300

    add bounce scroll animation

---
+++ src/components/AnimatedScroll.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function  () {
+  return (
+<div className='h-10 bg-red-500 mx-auto w-6 animate-bounce rounded-full'></div>
+  )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className='flex ml-2 mt-2'>
-        <h1 className='text-white italic text-2xl'>Jaguar.</h1>
-
+        <h1 className=' italic text-2xl text-red-500'>Jaguar.</h1>
     </div>
   )
 }
