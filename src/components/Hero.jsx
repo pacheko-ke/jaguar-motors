44bc83d502f97eae00c51de0641ac9ff65051c13
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 22:01:18 +0300

    adjust hero mt

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Jaguar from '../assets/jaguar.png'
 
 export default function() {
   return (
-    <div className='mt-16 flex justify-end'>
+    <div className='mt-24 flex justify-end'>
         <h1 className='text-white text-[130px] absolute mx-auto -mt-26'>Jaguar</h1>
         <img src={Jaguar} alt=" hero image" className='z-10 mt-10 scale' />
     </div>
