486325994d09ee01c33933f56af3819f4e318e5a
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 20:31:41 +0300

    Add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function() {
   return (
     <div className='mt-10 flex justify-end'>
         <h1 className='text-white text-[150px] absolute mx-auto -mt-26'>Jaguar</h1>
-        <img src={Jaguar} alt=" hero image" className='size-3/4 z-10 mt-10' />
+        <img src={Jaguar} alt=" hero image" className='z-10 mt-10' />
     </div>
   )
 }
