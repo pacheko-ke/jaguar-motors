d046bb305b73721f143859c4e27c9ec0ed5bd1c0
pacheko-ke <patrickoluochex@gmail.com>
2025-03-31 01:21:12 +0300

    add disclaimer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className='text-white flex flex-col gap-y-4 text-sm pt-10 px-4'>
+        <div className='pb-6 text-white flex flex-col gap-y-4 text-sm pt-10 px-4'>
             <div className='flex justify-between'>
                 <h1>RACING</h1>
                 <h1>+</h1>
