c3c2cecb16c73eb2add9869f3daf02c2de0fa54d
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 21:13:51 +0300

    change hero text-size

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function () {
   return (
     <div className='text-white mx-auto flex flex-col items-center'>
-        <h1>Sports Collection</h1>
-        <h1 className='tracking-normal'>NEW ARRIVALS</h1>
+        <h1 className='text-2xl'>Sports Collection</h1>
+        <h1 className='tracking-normal text-xl'>NEW ARRIVALS</h1>
 
 <div className='flex gap-x-2 mt-2'>
         <div className='size-2 bg-red-500 rounded-full'></div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Jaguar from '../assets/jaguar.png'
 export default function() {
   return (
     <div className='mt-10 flex justify-end'>
-        <h1 className='text-white text-[150px] absolute mx-auto -mt-26'>Jaguar</h1>
+        <h1 className='text-white text-[130px] absolute mx-auto -mt-26'>Jaguar</h1>
         <img src={Jaguar} alt=" hero image" className='z-10 mt-10' />
     </div>
   )
