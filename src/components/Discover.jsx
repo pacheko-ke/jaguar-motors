570d4003d486355748ee87ec3203d6a4cb209696
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 20:50:51 +0300

    add discover section

---
+++ src/components/Discover.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className='text-white mx-auto flex flex-col items-center'>
+        <h1>Sports Collection</h1>
+        <h1 className='tracking-normal'>NEW ARRIVALS</h1>
+
+<div className='flex gap-x-2 mt-2'>
+        <div className='size-2 bg-red-500 rounded-full'></div>
+        <div className='size-2 bg-white rounded-full'></div>
+        <div className='size-2 bg-white rounded-full'></div>
+    </div>
+    </div>
+  )
+}
