fdaf47b7a5997569a4d27a45ea4f6c79f4ba6782
pacheko-ke <patrickoluochex@gmail.com>
2025-03-31 01:25:38 +0300

    add plus icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,34 +5,45 @@ export default function Footer() {
         <div className='pb-6 text-white flex flex-col gap-y-4 text-sm pt-10 px-4'>
             <div className='flex justify-between'>
                 <h1>RACING</h1>
-                <h1>+</h1>
-                
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+
+
             </div>
             <div className='flex justify-between'>
-                
+
                 <h1>SPORTS CARS</h1>
-                <h1>+</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
             </div>
             <div className='flex justify-between'>
-                
+
                 <h1>COLLECTIONS</h1>
-                <h1>+</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
             </div>
             <div className='flex justify-between'>
-                
+
                 <h1>EXPERIENCE</h1>
-                <h1>+</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
             </div>
             <div className='flex justify-between'>
-                
+
                 <h1>ABOUT US</h1>
-                <h1>+</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
             </div>
 
 
             <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe deleniti necessitatibus hic vero. Corrupti similique minima, recusandae blanditiis praesentium saepe tenetur repellendus nulla in quisquam, architecto deleniti nobis soluta. Quis corporis culpa ipsa error aut deserunt, facilis delectus voluptates impedit molestiae! Omnis iusto necessitatibus expedita dolores dolor iste veritatis repellendus.</p>
             <h1 className='text-center text-sm'>Developed by Patrick.</h1>
         </div>
-        
+
     )
 }
