10a09644edbf0fd0badd88f07cd217e601f986d5
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 22:28:36 +0300

    update hero image for animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import '../App.css'
 
 export default function Navbar() {
   return (
-    <div className='flex pl-2 top-0 fixed z-20 bg-black shadow-lg w-full'>
-        <h1 className=' italic text-2xl text-red-500'>Jaguar.</h1>
+    <div className='flex pl-2 top-0 fixed z-20 bg-black shadow-lg w-full h-10'>
+        <h1 className=' italic text-2xl text-red-500 font-bold'>Jaguar.</h1>
     </div>
   )
 }
