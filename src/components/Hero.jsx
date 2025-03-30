43dce4d2cd796eb2c8cc24571afdea157a681d8b
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 22:46:07 +0300

    align items center

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function() {
     <div className='mt-24 flex flex-col justify-center items-center text-white'>
         <h1 className='text-white text-[130px] absolute mx-auto top-50 z-10'>Jaguar</h1>
         <img src={Jaguar} alt=" hero image" className=' rotate-180 -mt-50' />
-        <div className='absolute top-92 text-sm flex justify-center items-center align-center'>
+        <div className='absolute top-92 text-md flex justify-center items-center align-center'>
             <h1>In a world full of conventions...,</h1> <h1> copy nothing</h1>
             </div>
     </div>
