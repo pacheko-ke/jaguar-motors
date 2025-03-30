d768ebae359a4357ac9a15c0b37f221c8ca76eb6
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 23:56:56 +0300

    add animaion to home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function() {
         <h1 className='text-white text-[130px] absolute mx-auto top-50 z-10'>Jaguar</h1>
         <motion.img src={Jaguar} alt=" hero image" className='rotate-180 -mt-50' initial={{y:1000}} animate={{y:0}} transition={{duration:2}}></motion.img>
         <div className='absolute top-92 text-md flex justify-center items-center align-center'>
-            <h1>In a world full of conventions...,</h1> <h1> copy nothing</h1>
+            <motion.h1 initial={{x:-500}} animate={{x:0}} transition={{duration:1}}>In a world full of conventions...,</motion.h1> <motion.h1 initial={{x:500}} animate={{x:0}} transition={{duration:1}}> copy nothing</motion.h1>
             </div>
     </div>
   )
