23374a82cf308324e65f3d9c542c2dd03a576861
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 22:54:50 +0300

    add menu icon

--- src/components/AnimatedScroll.jsx
+++ src/components/AnimatedScroll.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function  () {
   return (
-<div className='  h-10 bg-red-500 mx-auto w-6 animate-bounce rounded-full'></div>
+<div className='  h-10 bg-red-500 mx-auto w-6 animate-bounce rounded-full'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-black py-2">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+</svg>
+</div>
   )
 }
