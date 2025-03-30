b036bb8ed0b8536149a02beea850c9c3150fe772
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 21:59:41 +0300

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import '../App.css'
 
 export default function Navbar() {
   return (
-    <div className='flex ml-2 mt-2'>
+    <div className='flex pl-2 top-0 fixed z-20 bg-black shadow-lg w-full'>
         <h1 className=' italic text-2xl text-red-500'>Jaguar.</h1>
     </div>
   )
