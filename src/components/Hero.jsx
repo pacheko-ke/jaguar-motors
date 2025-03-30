57b52e827ecb901c4e5ff6b2fe9006f0a1035b8d
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 22:39:59 +0300

    update hero image for animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,12 @@ import Jaguar from '../assets/jaguar-top.png'
 
 export default function() {
   return (
-    <div className='mt-24 flex justify-end'>
+    <div className='mt-24 flex flex-col justify-end text-white'>
         <h1 className='text-white text-[130px] absolute mx-auto top-50 z-10'>Jaguar</h1>
         <img src={Jaguar} alt=" hero image" className=' rotate-180 -mt-50' />
+        <div className='absolute top-92 text-xl flex'>
+            <h1>In a world full of conventions...,</h1> <h1> copy nothing</h1>
+            </div>
     </div>
   )
 }
