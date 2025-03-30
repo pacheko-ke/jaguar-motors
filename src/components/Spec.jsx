e1eddc100a98186a94a544c54c5b8eb409d74ac1
pacheko-ke <patrickoluochex@gmail.com>
2025-03-30 23:31:50 +0300

    adjust spacing

--- src/components/Spec.jsx
+++ src/components/Spec.jsx
@@ -3,9 +3,9 @@ import Engine from "../assets/engine.png"
 
 export default function () {
     return (
-        <div className='flex flex-col items-center text-white px-4 mt-2 gap-2'>
+        <div className='flex flex-col items-center text-white px-4 mt-6 gap-2'>
             {/* <img src={Engine} alt="enginwimage" className='absolute'/> */}
-            <p className=''>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem optio aliquam nobis totam neque aliquid accusamus nesciunt quis cupiditate eveniet, praesentium, dolores a recusandae quasi, architecto dolore similique nostrum! Pariatur.
+            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem optio aliquam nobis totam neque aliquid accusamus nesciunt quis cupiditate eveniet, praesentium, dolores a recusandae quasi, architecto dolore similique nostrum! Pariatur.
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-red-400">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                 </svg>
