19af0c6198e906142a6d90875e69aa737b18f7a7
pacheko-ke <patrickoluochex@gmail.com>
2025-03-31 09:23:39 +0300

    adjust padding on spec

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function () {
   return (
     <div className='text-white mx-auto flex flex-col items-center '>
-        <h1 className='text-md tracking-wider'>Sports Collection</h1>
-        <h1 className='tracking-widest text-sm'>NEW ARRIVALS</h1>
+        <h1 className='text-md md:text-xl tracking-wider'>Sports Collection</h1>
+        <h1 className='tracking-widest text-sm md:text-lg'>NEW ARRIVALS</h1>
 
 <div className='flex gap-x-2 mt-2'>
         <div className='size-2 bg-red-500 rounded-full'></div>
--- src/components/Spec.jsx
+++ src/components/Spec.jsx
@@ -4,9 +4,9 @@ import { motion } from "motion/react"
 
 export default function () {
     return (
-        <motion.div initial={{opacity:0,x:-200}} whileInView={{opacity:1,x:0}} transition={{duration:1}} viewport={{once:false}} className='flex flex-col items-center text-white px-4 mt-6 gap-2'>
+        <motion.div initial={{opacity:0,x:-200}} whileInView={{opacity:1,x:0}} transition={{duration:1}} viewport={{once:false}} className='flex flex-col items-center text-white px-4 md:px-20 mt-6 gap-2'>
             {/* <img src={Engine} alt="enginwimage" className='absolute'/> */}
-            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem optio aliquam nobis totam neque aliquid accusamus nesciunt quis cupiditate eveniet, praesentium, dolores a recusandae quasi, architecto dolore similique nostrum! Pariatur.
+            <p className='text-sm md:text-xl'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem optio aliquam nobis totam neque aliquid accusamus nesciunt quis cupiditate eveniet, praesentium, dolores a recusandae quasi, architecto dolore similique nostrum! Pariatur.
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-red-400">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                 </svg>
