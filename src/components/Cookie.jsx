9cf9510627f2876cb58c928c3f19ead1d73c25b4
pacheko-ke <patrickoluochex@gmail.com>
2025-03-31 10:19:36 +0300

    adjust cookie mobile responsiveness

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -9,9 +9,9 @@ export default function () {
     // },[])
   return (
     <div className={cookieAlreadyShown?'hidden':'bg-gray-900 flex flex-col items-center text-white py-6 gap-2 px-2 absolute fixed bottom-10 z-20 shadow-2xl mr-4'}>
-        <p className='text-black text-sm md:text-lg w-full md:w-1/2 text-center'>This website uses cookies to improve your experience across the site. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, eum. </p>
+        <p className='text-white text-sm md:text-lg w-full md:w-1/2 text-center'>This website uses cookies to improve your experience across the site. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, eum. </p>
         <div className="flex gap-x-2 w-full items-center justify-center">
-        <button className='bg-black w-1/2 md:w-1/4 py-2'>Accept</button><button className='bg-transparent border-1 text-black border border-red-500 w-1/2 md:w-1/4 py-2'>Decline</button>
+        <button className='bg-black w-1/2 md:w-1/4 py-2'>Accept</button><button className='bg-transparent border-1 text-white border border-red-500 w-1/2 md:w-1/4 py-2'>Decline</button>
         </div>
     </div>
   )
