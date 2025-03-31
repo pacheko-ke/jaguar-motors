5be3d5e38329522036a2efdf5db37f3bdc1f5434
pacheko-ke <patrickoluochex@gmail.com>
2025-03-31 10:23:38 +0300

    close cookie consent on accept and reject

--- src/components/Cookie.jsx
+++ src/components/Cookie.jsx
@@ -11,7 +11,7 @@ export default function () {
     <div className={cookieAlreadyShown?'hidden':'bg-gray-900 flex flex-col items-center text-white py-6 gap-2 px-2 absolute fixed bottom-10 z-20 shadow-2xl mr-4'}>
         <p className='text-white text-sm md:text-lg w-full md:w-1/2 text-center'>This website uses cookies to improve your experience across the site. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, eum. </p>
         <div className="flex gap-x-2 w-full items-center justify-center">
-        <button className='bg-black w-1/2 md:w-1/4 py-2'>Accept</button><button className='bg-transparent border-1 text-white border border-red-500 w-1/2 md:w-1/4 py-2'>Decline</button>
+        <button className='bg-black w-1/2 md:w-1/4 py-2' onClick={()=>showCookie(!cookieAlreadyShown)}>Accept</button><button className='bg-transparent border-1 text-white border border-red-500 w-1/2 md:w-1/4 py-2' onClick={()=>showCookie(!cookieAlreadyShown)}>Decline</button>
         </div>
     </div>
   )
